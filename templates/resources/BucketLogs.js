6ef1cc1c28130903c8cf0b37988bb2d928c2bf75
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2017-11-12 09:39:40 +0900

    Add s3 bucket for logs

---
+++ templates/resources/BucketLogs.js
@@ -0,0 +1,26 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/* eslint-env es6, mocha, node */
+/* eslint-extends: eslint:recommended */
+"use strict";
+
+
+
+// Export module
+module.exports = {
+    Type: 'AWS::S3::Bucket',
+    Properties: {
+        BucketName: 'logs.honoka.io',
+        AccessControl: 'Private'
+    }
+};
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
